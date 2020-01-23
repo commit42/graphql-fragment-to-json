0e4542374fe8d1e74dc90f87cf3faa64c885d3f5
ozee31 <flavien.beninca@gmail.com>
2020-01-23 14:20:39 +0100

    feat(index): create and test index function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-export default () => {
-  return true;
+import _set from "lodash.set";
+import isFragment from "./isFragment";
+
+const DEFAULT_VALUE = undefined;
+
+const convertSelectionsToObject = (
+  selections,
+  defaultValue,
+  finalObject = {},
+  namespace = ""
+) => {
+  if (!Array.isArray(selections)) {
+    return finalObject;
+  }
+
+  selections.forEach(selection => {
+    const fieldName = selection?.name?.value;
+
+    if (!fieldName) {
+      return false;
+    }
+
+    const completeFieldName = namespace.length
+      ? `${namespace}.${fieldName}`
+      : fieldName;
+
+    _set(finalObject, completeFieldName, defaultValue);
+
+    const hasSubSelections = !!selection?.selectionSet?.selections;
+
+    if (hasSubSelections) {
+      const currentNamespace = completeFieldName;
+      const subSelections = selection.selectionSet.selections;
+      finalObject = convertSelectionsToObject(
+        subSelections,
+        defaultValue,
+        finalObject,
+        currentNamespace
+      );
+    }
+  });
+
+  return finalObject;
+};
+
+export default (fragment, defaultValue = DEFAULT_VALUE) => {
+  if (!isFragment(fragment)) {
+    return null;
+  }
+
+  const selections = fragment?.selectionSet?.selections;
+
+  if (!selections) {
+    return null;
+  }
+
+  return convertSelectionsToObject(selections, defaultValue);
 };
